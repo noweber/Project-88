9ec02fd7dabd6f3818ffd067f881f7181ad34ab3
Nicholas Owen Weber <noweber@gmail.com>
2019-12-11 01:53:33 -0600

    Update AzureEventHubRepository.cs
    This commit adds comments to the AEH repository.

--- Project88/AzureEventHubRepository.cs
+++ Project88/AzureEventHubRepository.cs
@@ -10,8 +10,8 @@ namespace Project88.Message.Producer
         /// <summary>
         /// Constructs for an AzureEventHubRepository object.
         /// </summary>
-        /// <param name="eventHubConnectionString">{Event Hubs connection string}</param>
-        /// <param name="eventHubName">{Event Hub path/name}</param>
+        /// <param name="eventHubConnectionString">{Event Hubs connection string} which contains the credentials to connect to a specific instance of Event Hubs.</param>
+        /// <param name="eventHubName">{Event Hub path/name} which is the messaging topic.</param>
         public AzureEventHubRepository(string eventHubConnectionString, string eventHubName)
         {
             EventHubsConnectionStringBuilder connectionStringBuilder = new EventHubsConnectionStringBuilder(eventHubConnectionString)
@@ -22,6 +22,10 @@ namespace Project88.Message.Producer
             eventHubClient = EventHubClient.CreateFromConnectionString(connectionStringBuilder.ToString());
         }
 
+        /// <summary>
+        /// Publishes a message to Azure Event Hubs using the connection string and topic from the object's constructor.
+        /// </summary>
+        /// <param name="message">The message which will be published to Azure Event Hubs.</param>
         public void PublishEventMessage(string message)
         {
             eventHubClient.SendAsync(new EventData(Encoding.UTF8.GetBytes(message)));
